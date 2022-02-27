a6a87716807e4c8181ee18ada13a5ab8caef9c62
Brandon Lyons <brandonl.4231@gmail.com>
2022-02-26 22:05:11 -0500

    v1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,57 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="getHelp(true)">I need help</button>
+    <button @click="getHelp(false)">Nevermind, I got it</button>
+    <img v-if="needsHelp" style="width:480px" src="https://media1.giphy.com/media/phJ6eMRFYI6CQ/giphy.gif?cid=ecf05e476dhtlxuyn1kl5ac9npgtl6ft5whahsrrhmc9sbff&rid=giphy.gif&ct=g"/>
+  
   </div>
 </template>
 
 <script>
+import PieSocket from 'piesocket-js'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+      socket: null,
+      channel: null,
+      needsHelp: false
+    }
+  },
+  methods: {
+    getHelp(payload) {
+      this.channel.publish('help-me', payload)
+    },
+    async configureSocket() {
+      const channel = await this.socket.subscribe('mitzi-alert-test')
+      channel.listen('help-me', (data, meta) => {
+      console.log('help-me received', data, meta)
+      this.needsHelp = data
+    })
+    this.channel = channel
+    }
+  },
+  async mounted() {
+    this.socket = new PieSocket({
+      clusterId: 'demo',
+      apiKey: 'oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm'
+    })
+    this.configureSocket()
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+img {
+  display: block;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+button {
+  padding: .5em;
+  margin: .5em;
 }
 </style>
